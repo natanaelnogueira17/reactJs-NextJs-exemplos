8a78e347794b9506350428e13ec5a1cecea1bdbd
Natanael Nogueira <102445138+natanaelnogueira17@users.noreply.github.com>
2022-06-28 07:08:58 -0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import Title from '../src/componentes/title/title'
+
+
+function HomePage(){
+  return (
+    <div>
+      <Title/>
+      
+      Hello World, NextJs + ReactJs23
+    </div>
+  )
+}
+
+export default HomePage
---
+++ title.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+function Title(){
+  return(
+    <h1> Meu Titulo</h1>
+  )
+}
+
+export default Title
